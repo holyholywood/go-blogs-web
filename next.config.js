9ceafece8cee20c0710cf05985d4e6df9d642449
Ditotisi Rasyid Sumpena <ditolord@gmail.com>
2023-08-25 22:46:31 +0700

    add config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["via.placeholder.com", "dummyimage.com", "i.pravatar.cc", "ui-avatars.com", "127.0.0.1", "localhost"],
+    domains: ["via.placeholder.com", "dummyimage.com", "i.pravatar.cc", "ui-avatars.com", "127.0.0.1", "localhost", "api-ngeblog.ditotisi.com"],
     minimumCacheTTL: 1500000,
   },
 };
